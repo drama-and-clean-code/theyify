5547691c746beb71931c7f80ddce93890f780232
Jackie Lealess <jlealess@gmail.com>
2018-07-19 19:11:52 -0400

    Adding initial pass at language processing rules

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-var allImages = document.getElementsByTagName('img');
-console.log(allImages)
-for (var i = 0; i < allImages.length; i++) {
-	var imageHeight = allImages[i].clientHeight;
-	var imageWidth = allImages[i].clientWidth;
-	allImages[i].setAttribute('src', 'http://placekitten.com/' + imageWidth + '/' + imageHeight)
-}
+const elements = document.getElementsByTagName('*');
 
-document.body.style = "transition: 2s; transform: rotate(1080deg);";
+for (let i=0; i < elements.length; i++) {
+	let element = elements[i];
+
+	for (let j=0; j < element.childNodes.length; j++) {
+		let node = element.childNodes[j];
+
+		if (node.nodeType === 3) {
+			let text = node.nodeValue;
+			let replacedText = text
+			.replace(/\bs\/he\b/g, "they")
+			.replace(/\bhe(\/)?(\sor\s)?she\b/g, "they")
+        	.replace(/\b(he|she)\b/g, "they")
+			.replace(/\b(He|She)\b/g, "They")
+			.replace(/\b(he's|she's)\b/g, "they're")
+			.replace(/\b(He's|She's)\b/g, "They're")
+			.replace(/\b(hers|his)\b/g, "theirs")
+			.replace(/\b(Hers|His)\b/g, "Theirs")
+			.replace(/\b(to|from|at|for)\b\s\b(Her|Him)\b/g, "$1 Them")
+			.replace(/\b(to|from|at|for)\b\s\b(her|him)\b/g, "$1 them")
+			.replace(/\b(her|him)\b/g, "their")
+			.replace(/\b(Her|Him)\b/g, "Their")
+			/*  
+			figure out verbs
+			-ing her/him => $1 them
+			*/
+			if (replacedText !== text) {
+				element.replaceChild(document.createTextNode(replacedText), node);
+			}
+		}
+
+	}
+}
